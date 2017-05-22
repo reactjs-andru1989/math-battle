4be38f6f5bab512f01e5951b0143c33a4d018f3b
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-05-22 02:32:21 -0500

    remove firebase credentials

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as firebase from 'firebase';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
@@ -10,16 +9,6 @@ import reducers from './reducers';
 import Home from './components/home';
 import Board from './components/board';
 import Score from './components/score';
-
-const config = {
-  apiKey:             "AIzaSyDhYq2bHovtm5Eq55kckCgXwq1FcWk9pcU",
-  authDomain:         "math-battle-10719.firebaseapp.com",
-  databaseURL:        "https://math-battle-10719.firebaseio.com",
-  projectId:          "math-battle-10719",
-  storageBucket:      "math-battle-10719.appspot.com",
-  messagingSenderId:  "729210680536"
-};
-firebase.initializeApp(config);
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
