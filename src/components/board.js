44ff764c6be3092ce13d8cfc48c02690a4483675
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-05-22 02:31:44 -0500

    fix board to show Countdown and MathOperation

--- src/components/board.js
+++ src/components/board.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import Countdown from './countdown';
+import MathOperation from '../containers/math_operation';
+import { Link } from 'react-router-dom';
 
 class Board extends Component {
+  finishGame() {
+    this.props.history.push('/')
+  }
+
   render() {
     return (
-      <h1>Board</h1>
+      <div>
+        <Link to="/">Finish</Link>
+        <h1>Board</h1>
+        <Countdown secondsRemaining="20" finishGame={() => this.finishGame()} />
+        <MathOperation />
+      </div>
     );
   }
 }
