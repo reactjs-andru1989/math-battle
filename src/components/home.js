0171b4e4b5d05b55403be13896a31e9e92157b70
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-05-22 02:31:59 -0500

    remove Countdown from home

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Countdown from './countdown';
 
 class Home extends Component {
   render() {
@@ -9,7 +8,6 @@ class Home extends Component {
         <div className="text-center">
           <Link to="/board">Play</Link>
           <Link to="/score">Score</Link>
-          <Countdown secondsRemaining="10" />
         </div>
       </div>
     );
