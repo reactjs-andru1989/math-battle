2a334a39cbf3fc79062aad556fe90c179414ed04
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-05-22 02:33:01 -0500

    call reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import mathOperation from './reducer_math_operation';
+import checkOperation from './reducer_check_operation';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  activeOperation: mathOperation,
+  checkOperation: checkOperation
 });
 
 export default rootReducer;
