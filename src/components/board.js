56b663b714076db1ed9f899a93caf4c0da4759ad
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-05-25 01:14:40 -0500

    temporal commit

--- src/components/board.js
+++ src/components/board.js
@@ -7,7 +7,7 @@ class Board extends Component {
   constructor(props) {
     super(props)
     this.state = ({
-      secondsRemaining: 20,
+      secondsRemaining: 10,
       score: 0
     })
   }
@@ -25,11 +25,21 @@ class Board extends Component {
   }
 
   addSeconds() {
-    this.setState({ secondsRemaining: this.state.secondsRemaining + 5 });
+    const maxSecondsRemaining = 20;
+    let secondsToAdd = 5;
+    let nextSecondsRemaining = this.state.secondsRemaining + secondsToAdd
+    if (nextSecondsRemaining > 20) {
+      secondsToAdd = maxSecondsRemaining - this.state.secondsRemaining;
+      this.setState({ secondsRemaining: this.state.secondsRemaining + secondsToAdd });
+    } else {
+      this.setState({ secondsRemaining: this.state.secondsRemaining + secondsToAdd });
+    }
   }
 
   subtractSeconds() {
-    this.setState({ secondsRemaining: this.state.secondsRemaining - 5 });
+    const secondsToSubtract = 6;
+    let seconds = this.state.secondsRemaining >= secondsToSubtract ? secondsToSubtract : this.state.secondsRemaining
+    this.setState({ secondsRemaining: this.state.secondsRemaining - seconds });
   }
 
   addScore() {
@@ -49,6 +59,11 @@ class Board extends Component {
           addSeconds      = {() => this.addSeconds()}
           subtractSeconds = {() => this.subtractSeconds()}
           addScore        = {() => this.addScore()} />
+        <div className="progress">
+          <div className="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow={this.state.secondsRemaining} aria-valuemin={10} aria-valuemax={20} style={{width: this.state.secondsRemaining*100/20 + "%"}}>
+            <span className="sr-only">{this.state.secondsRemaining}% Complete</span>
+          </div>
+        </div>
       </div>
     );
   }
--- src/containers/math_operation.js
+++ src/containers/math_operation.js
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { randomOperation, checkOperation } from '../actions/index';
-import { bindActionCreators } from 'redux';
+import { OPERATIONS, RESULT_TYPES } from '../actions/types';
 
 class MathOperation extends Component {
+  constructor(props) {
+    super(props)
+    this.state = ({message: '', operation: '', result: 0})
+    this.randomOperation = this.randomOperation.bind(this)
+  }
+
   componentDidMount() {
-    this.props.randomOperation()
+    this.randomOperation()
+  }
+
+  randomOperation() {
+    let operation = OPERATIONS[Math.floor(Math.random() * OPERATIONS.length)];
+
+    let n1 = Math.floor(Math.random() * 10) + 1
+    let n2 = Math.floor(Math.random() * 10) + 1
+
+    let result_value;
+    let result_type = RESULT_TYPES[Math.floor(Math.random() * RESULT_TYPES.length)];
+
+    if (result_type === 'truthy') {
+      result_value = n1 + n2
+    } else {
+      result_value = n1 + n2 + 5
+    }
+
+    this.setState({
+      message: `${n1} + ${n2} = ${result_value}`,
+      operation: `${n1} + ${n2}`,
+      result: result_value
+    })
+  }
+
+  checkOperation(answer) {
+    let result = (eval(this.state.operation) === this.state.result)
+    if (result === answer) {
+      this.props.addSeconds()
+      this.props.addScore()
+    } else {
+      this.props.subtractSeconds()
+    }
+    this.randomOperation()
   }
 
   render() {
-    if (!this.props.operation) {
+    if (!this.state.operation) {
       return <div>Loading...</div>
     }
 
     return (
       <div>
-        <button onClick={this.props.randomOperation}>Random</button>
-        {this.props.operation.message}
-        <button
-          onClick={() => this.props.checkOperation(true, this.props)}>
+        <button onClick={this.randomOperation}>Random</button>
+        {this.state.message}
+        <button onClick={() => this.checkOperation(true)}>
           Verdadero
         </button>
-        <button
-          onClick={() => this.props.checkOperation(false, this.props)}>
+        <button onClick={() => this.checkOperation(false)}>
           Falso
         </button>
       </div>
@@ -30,17 +65,4 @@ class MathOperation extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    operation: state.activeOperation
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    randomOperation: randomOperation,
-    checkOperation: checkOperation
-  }, dispatch)
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(MathOperation);
+export default MathOperation;
