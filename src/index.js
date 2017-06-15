b0663273d634d808db5bc2bf03746ca059e6bb3b
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-06-15 01:44:47 -0500

    add redux-promise

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import reducers from './reducers';
 import Home from './components/home';
 import Board from './components/board';
 import Score from './components/score';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
