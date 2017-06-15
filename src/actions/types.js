a9a08a53b8086e6abccefb92732ad90cb67297c2
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-06-15 01:39:40 -0500

    add type

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,3 +2,4 @@ export const OPERATIONS = ['addition', 'addition', 'addition'];
 export const CHECKING = 'checking';
 export const ADDITION = 'addition';
 export const RESULT_TYPES = ['truthy', 'falsey'];
+export const FETCH_USERS = 'FETCH_USERS';
